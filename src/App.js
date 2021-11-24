9aaa0ba1428672c751a5b808ba2a0fa1e242308f
Amrhub <amr_ahmed65@hotmail.com>
2021-11-25 00:24:47 +0200

    refactor: class-based comp. to functional comp.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.scss';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App flex-center-center">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App flex-center-center">
+    <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/CalculatorKeypadRow.js
+++ src/components/CalculatorKeypadRow.js
@@ -1,40 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class KeypadRow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ClickHandler = this.clickHandler.bind(this);
-  }
+const KeypadRow = (props) => {
+  const { values } = props;
+  const { callCalculate } = props;
 
-  clickHandler = (e) => {
+  const clickHandler = (e) => {
     const ButtonName = e.target.innerText;
-    const { callCalculate } = this.props;
     callCalculate(ButtonName);
   };
-
-  render() {
-    const { values } = this.props;
-
-    return (
-      <div className="calculator__keypad-row">
-        {values.map((value) => (
-          <button
-            className="calculator__keypad-key flex-center-center"
-            type="button"
-            key={value}
-            onClick={this.clickHandler}
-          >
-            {value}
-          </button>
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calculator__keypad-row">
+      {values.map((value) => (
+        <button
+          className="calculator__keypad-key flex-center-center"
+          type="button"
+          key={value}
+          onClick={clickHandler}
+        >
+          {value}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 KeypadRow.propTypes = {
   values: PropTypes.arrayOf(PropTypes.string).isRequired,
   callCalculate: PropTypes.func.isRequired,
 };
+
+export default KeypadRow;
