42e04f482542601df371dbbe1ae6863420a97303
Amrhub <amr_ahmed65@hotmail.com>
2021-11-24 03:21:50 +0200

    fix: make App class-based

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App flex-center-center">
-      <Calculator />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App flex-center-center">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
