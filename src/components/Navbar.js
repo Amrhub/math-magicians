1a38ba4a4c2cb3ffa2ba1ad2544d5f8ab626e7f8
Amrhub <amr_ahmed65@hotmail.com>
2021-12-01 00:13:49 +0200

    feat: add navbar and style

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="navbar flex-sb-center">
+    <span className="navbar-brand">math magicians</span>
+
+    <ul className="navbar-nav">
+      <li>
+        <a href="" className="nav-link">
+          Home
+        </a>
+        <a href="" className="nav-link">
+          Calculator
+        </a>
+        <a href="" className="nav-link">
+          Quote
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
