436843fac1f6ccd949af0c7dce667a87bee1203c
Amrhub <amr_ahmed65@hotmail.com>
2021-12-01 00:34:46 +0200

    feat: add home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.scss';
 import Navbar from './components/Navbar';
+import Quote from './pages/Quote/Quote';
+import Home from './pages/Home/Home';
 
 const App = () => (
   <div className="App">
     <Navbar />
     <Calculator />
+    <Quote />
+    <Home />
   </div>
 );
 
