e4ba5e0a86f0b5b8fa0af4858f1f0f494edfbf17
Nicolae Pop <nicupop729@gmail.com>
2021-12-01 19:58:51 -0300

    feat: calculate tests

--- src/logic/__tests__/logic.test.js
+++ src/logic/__tests__/logic.test.js
@@ -9,7 +9,7 @@ describe('calculate.js', () => {
     expect(isNumber('z')).not.toBeTruthy();
   });
 
-  it('calculate() should return null values of object when second param is "AC" ', () => {
+  it('calculate({operation, next}, buttonName=AC) should return { total: null, next: null, operation: null }', () => {
     const buttonName = 'AC';
     const obj = { total: 0, next: 0, operation: null };
 
@@ -17,70 +17,194 @@ describe('calculate.js', () => {
 
     expect(result).toEqual({ total: null, next: null, operation: null });
   });
+
+  it('calculate({operation, next:0}, buttonName=0) should return {}', () => {
+    const buttonName = '0';
+    const obj = { total: null, next: '0', operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({});
+  });
+
+  it('calculate({operation, next}, buttonName) should override obj.next: obj.next+buttonName', () => {
+    const buttonName = '5';
+    const obj = { total: null, next: '3', operation: '+' };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ ...obj, next: obj.next + buttonName });
+  });
+
+  it('calculate({operation, !next}, buttonName) should override obj.next: buttonName', () => {
+    const buttonName = '5';
+    const obj = { total: null, next: null, operation: '+' };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ ...obj, next: buttonName });
+  });
+
+  it('calculate({!operation, next}, buttonName) should return next: obj.next + buttonName', () => {
+    const buttonName = '5';
+    const obj = { total: null, next: '7', operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ total: null, next: obj.next + buttonName });
+  });
+
+  it('calculate({!operation, !next}, buttonName) should return next: buttonName', () => {
+    const buttonName = '5';
+    const obj = { total: null, next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ next: buttonName, total: null });
+  });
+
+  it('calculate({next.includes(".")}, buttonName=".") should return same object', () => {
+    const buttonName = '.';
+    const obj = { total: null, next: '5.3', operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual(obj);
+  });
+
+  it('calculate({!next.includes(".")}, buttonName=".") should override next, obj.next+"."', () => {
+    const buttonName = '.';
+    const obj = { total: null, next: '53', operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ ...obj, next: obj.next + buttonName });
+  });
+
+  it('calculate({operation}, buttonName=".") should return { next: "0." }', () => {
+    const buttonName = '.';
+    const obj = { total: null, next: null, operation: '+' };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ next: 0 + buttonName });
+  });
+
+  it('calculate({total.includes(".")}, buttonName=".") should return {}', () => {
+    const buttonName = '.';
+    const obj = { total: '4.3', next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({});
+  });
+
+  it('calculate({!total.includes(".")}, buttonName=".") should return { total: "total." }', () => {
+    const buttonName = '.';
+    const obj = { total: '43', next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ total: `${obj.total}.` });
+  });
+
+  it('calculate({!total}, buttonName=".") should return { total: "total." }', () => {
+    const buttonName = '.';
+    const obj = { total: null, next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ total: 0 + buttonName });
+  });
+
+  it('calculate({next:"5", total:"10", operation:"-"}, buttonName="=") should return { total: "5" }', () => {
+    const buttonName = '=';
+    const obj = { total: '10', next: '5', operation: '-' };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ next: null, total: '5', operation: null });
+  });
+
+  it('calculate({next:"5", total:"10", operation:null}, buttonName="=") should return {}', () => {
+    const buttonName = '=';
+    const obj = { total: '10', next: '5', operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({});
+  });
+
+  it('calculate({next}, buttonName="+/-") should override next: -obj.next', () => {
+    const buttonName = '+/-';
+    const obj = { total: '10', next: '5', operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({
+      ...obj,
+      next: (-1 * parseFloat(obj.next)).toString(),
+    });
+  });
+
+  it('calculate({total}, buttonName="+/-") should override total: -obj.total', () => {
+    const buttonName = '+/-';
+    const obj = { total: '10', next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({
+      ...obj,
+      total: (-1 * parseFloat(obj.total)).toString(),
+    });
+  });
+
+  it('calculate({!next, !total}, buttonName="+/-") should return {}', () => {
+    const buttonName = '+/-';
+    const obj = { total: null, next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({});
+  });
+
+  it('calculate({!next, !total}, buttonName="+") should return {}', () => {
+    const buttonName = '+';
+    const obj = { total: null, next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({});
+  });
+
+  it('calculate({!next, total, !operation}, buttonName="+") should override operation: buttonName', () => {
+    const buttonName = '+';
+    const obj = { total: '5', next: null, operation: null };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ ...obj, operation: buttonName });
+  });
+
+  it('calculate({!next, total, operation}, buttonName="+") should override operation: buttonName', () => {
+    const buttonName = '+';
+    const obj = { total: '5', next: null, operation: '-' };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({ ...obj, operation: buttonName });
+  });
+
+  it('calculate({next, total, operation}, buttonName="+") should override operation: buttonName', () => {
+    const buttonName = '+';
+    const obj = { total: '5', next: '34', operation: '-' };
+
+    const result = calculate(obj, buttonName);
+
+    expect(result).toStrictEqual({
+      total: '-29',
+      next: null,
+      operation: buttonName,
+    });
+  });
 });
-
-// number
-// if pass zero when we already had zero in next return empty object
-// if there is a operation 
-   // if there's a obj.next 
-      // concat the obj.next with the number
-  // else no obj.next
-      // set obj.next to the number
-
-// else no operation
-  // if there's a obj.next
-    // concat the obj.next with the number
-  // else no obj.next
-    // set obj.next to the number
-
-// buttonName = "."
-  // if there's a obj.next
-    // if it includes a dot
-      // return same object
-    // else
-      // concat the obj.next with the buttonName ('.') 
-
-  // if there's operation
-    // return { next: '0.' }
-
-  // if we have total
-    // includes a dot return {}
-    // doesn't include a dot return { total: 'total.' }
-
-  // if we have no total
-    // return { total: '0.' }
-
-  // if buttonName = "="
-    // if obj.next and obj.operation
-      // { total: call calculate(obj.total, obj.next, obj.operation) 
-          // next: null
-          // operation: null
-        // }
-    // else no operation 
-      // return {}
-
-  // if buttonName = "+/-" 
-    // if obj.next
-      // return the obj and override the negative of next
-    // if obj.total
-      // return the obj and override the negative of total
-    // else no obj.next or obj.total
-      // return {}
-
-  // if we have operation
-    // if no obj.next or obj.total
-      // return {}
-
-  // User pressed an operation after pressing '='
-    // return obj and override the operation
-
-  // User pressed an operation button and there is an existing operation
-    // if we have total and no next
-      // return obj and override the operation
-    // if we have total and next
-      // return { total: call calculate(obj.total, obj.next, obj.operation)
-        // next: null
-        // operation: buttonName
-      // }
-
-      
