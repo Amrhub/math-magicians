9963b803609e1b8f0bf06e2be9cf9629839ace0b
Nicolae Pop <nicupop729@gmail.com>
2021-12-03 19:29:05 -0300

    feat: user interaction tests

--- src/components/__tests__/Calculator.test.js
+++ src/components/__tests__/Calculator.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Calculator from '../Calculator';
 import CalculatorKeypadRow from '../CalculatorKeypadRow';
@@ -30,10 +31,34 @@ describe('Calculator user interaction', () => {
   it('when user hits a "7" it renders "7"', () => {
     render(<Calculator />);
 
-    fireEvent.click(screen.getByText('7'));
+    userEvent.click(screen.getByText(/7/i));
 
-    // const display = screen.getByTestId('display');
+    const display = screen.getByTestId('display');
 
-    // expect(display.textContent).toBe('7');
+    expect(display).toHaveTextContent(/7/i);
+  });
+
+  it('when user hits a "4" then hits "5" it renders "45"', () => {
+    render(<Calculator />);
+
+    userEvent.click(screen.getByText(/4/i));
+    userEvent.click(screen.getByText(/5/i));
+
+    const display = screen.getByTestId('display');
+
+    expect(display).toHaveTextContent(/45/i);
+  });
+
+  it('when user hits a "4" then hits "+" then hits "5" then hits "=" it renders "9"', () => {
+    render(<Calculator />);
+
+    userEvent.click(screen.getByText(/4/i));
+    userEvent.click(screen.getByText('+'));
+    userEvent.click(screen.getByText(/5/i));
+    userEvent.click(screen.getByText('='));
+
+    const display = screen.getByTestId('display');
+
+    expect(display).toHaveTextContent(/9/i);
   });
 });
